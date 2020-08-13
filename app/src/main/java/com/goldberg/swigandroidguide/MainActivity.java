9b81c735651a8587184c6a10b5e2718408a590e8
Alex Goldberg <radio.rat.fm@gmail.com>
2020-08-13 19:03:15 +0300

    Initial commit.

---
+++ app/src/main/java/com/goldberg/swigandroidguide/MainActivity.java
@@ -0,0 +1,31 @@
+package com.goldberg.swigandroidguide;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    static
+    {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
